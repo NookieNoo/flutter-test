b4f50cf3cf006e7a2ed06ef4e8bf154eedb70dda
NookieNoo <biv0208@yandex.ru>
2021-07-14 23:36:46 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+      child: Text(
+    "Hello Flutter",
+    textDirection: TextDirection.ltr,
+    style: TextStyle(fontSize: 30, color: Colors.red, fontWeight: FontWeight.bold),
+  )));
+}
