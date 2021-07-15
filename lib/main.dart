c56f40422d7ba721805fc6d244158bc3d0523647
NookieNoo <biv0208@yandex.ru>
2021-07-15 23:34:41 +0300

    Кастомный шрифт

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'IndieFlower'),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Adding Assets"),
@@ -18,7 +19,18 @@ class MyApp extends StatelessWidget {
               Image(
                 image: AssetImage('assets/images/bg.jpg'),
               ),
-              Image.asset('assets/icons/icon.png')
+              Image.asset('assets/icons/icon.png'),
+              Positioned(
+                top: 16,
+                left: 115,
+                child: Text(
+                  'My custom Font',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              )
             ],
           ),
         ),
