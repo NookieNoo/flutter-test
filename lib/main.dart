6cfae60045abcc128f3119baf990dcb8ea062798
NookieNoo <biv0208@yandex.ru>
2021-07-27 17:00:44 +0300

    Первая верстка интерфейса(Погода)

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,164 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyCounterApp());
+void main() => runApp(MyApp());
 
-class MyCounterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.blue[300],
-      appBar: AppBar(
-        title: Text('CounterApp',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            )),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            textDirection: TextDirection.ltr,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                child: Text(
-                  'Tap "+" decrement',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              CounterWidget(),
-              Container(
-                child: Text(
-                  'Tap "-" increment',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Weather',
+            style: TextStyle(color: Colors.black87),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          iconTheme: IconThemeData(color: Colors.black54),
+          brightness: Brightness.light,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
         ),
+        body: _buildBody(),
       ),
-    ));
-  }
-}
-
-class CounterWidget extends StatefulWidget {
-  @override
-  _CounterWidgetState createState() => _CounterWidgetState();
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _count = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8.0),
-        color: Colors.blue[100],
-      ),
-      margin: EdgeInsets.only(left: 140, right: 140, top: 5, bottom: 5),
-      child: Row(
-          textDirection: TextDirection.ltr,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-                icon: Icon(Icons.remove),
-                onPressed: () {
-                  setState(() {
-                    _count -= 1;
-                  });
-                }),
-            Text('${(_count)}',
-                style: TextStyle(
-                  fontSize: 30,
-                )),
-            IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  setState(() {
-                    _count += 1;
-                  });
-                }),
-          ]),
     );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget>[
+        _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _weatherDescription(),
+                Divider(),
+                _temperature(),
+                Divider(),
+                _temperatureForecast(),
+                Divider(),
+                _footerRatings(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Image _headerImage() {
+  return Image(
+    image: NetworkImage(
+        'https://studychinese.ru/content/dictionary/pictures/25/12985.jpg'),
+    fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        'Tuesday - May 22',
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+      Divider(),
+      Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+          style: TextStyle(color: Colors.black54))
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(Icons.wb_sunny, color: Colors.yellow),
+        ],
+      ),
+      SizedBox(
+        width: 16,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                '15* Clear',
+                style: TextStyle(color: Colors.deepPurple),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                'Voronezhskaya obl, Voronezh',
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          ),
+        ],
+      )
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10,
+    children: List.generate(
+      8,
+      (int index) => Chip(
+        label: Text(
+          '${index + 20}C',
+          style: TextStyle(fontSize: 15),
+        ),
+        avatar: Icon(Icons.wb_cloudy, color: Colors.blue.shade300),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      ),
+    ),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Icon(Icons.star, size: 15, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15, color: Colors.black),
+      Icon(Icons.star, size: 15, color: Colors.black),
+    ],
+  );
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[
+      Text('Info with openweather', style: TextStyle(fontSize: 15)),
+      stars,
+    ],
+  );
 }
