511d9a7d8b75e6271cc285f96882cf2c379c69f7
NookieNoo <biv0208@yandex.ru>
2021-07-27 19:20:10 +0300

    Работа со статическим ListView

--- lib/main.dart
+++ lib/main.dart
@@ -9,156 +9,49 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'Weather',
-            style: TextStyle(color: Colors.black87),
+            'Building list view',
           ),
           centerTitle: true,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          iconTheme: IconThemeData(color: Colors.black54),
-          brightness: Brightness.light,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {},
-            ),
-          ],
         ),
-        body: _buildBody(),
+        body: BodyListView(),
       ),
     );
   }
 }
 
-Widget _buildBody() {
-  return SingleChildScrollView(
-    child: Column(
-      children: <Widget>[
-        _headerImage(),
-        SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                _weatherDescription(),
-                Divider(),
-                _temperature(),
-                Divider(),
-                _temperatureForecast(),
-                Divider(),
-                _footerRatings(),
-              ],
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
+class BodyListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
 }
 
-Image _headerImage() {
-  return Image(
-    image: NetworkImage(
-        'https://studychinese.ru/content/dictionary/pictures/25/12985.jpg'),
-    fit: BoxFit.cover,
-  );
-}
-
-Column _weatherDescription() {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
+Widget _myListView() {
+  return ListView(
+    padding: EdgeInsets.all(8.0),
+    // shrinkWrap: true,
+    // scrollDirection: Axis.horizontal,
+    // itemExtent: 300,
+    // reverse: true,
     children: <Widget>[
-      Text(
-        'Tuesday - May 22',
-        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ListTile(
+        title: Text('Sun'),
+        subtitle: Text('Today Clear'),
+        leading: Icon(Icons.wb_sunny),
+        trailing: Icon(Icons.keyboard_arrow_right),
       ),
-      Divider(),
-      Text(
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-          style: TextStyle(color: Colors.black54))
-    ],
-  );
-}
-
-Row _temperature() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Icon(Icons.wb_sunny, color: Colors.yellow),
-        ],
+      ListTile(
+        title: Text('Clowdy'),
+        subtitle: Text('Today Clowdy'),
+        leading: Icon(Icons.wb_cloudy),
+        trailing: Icon(Icons.keyboard_arrow_right),
       ),
-      SizedBox(
-        width: 16,
+      ListTile(
+        title: Text('Snow'),
+        subtitle: Text('Today Snow'),
+        leading: Icon(Icons.ac_unit),
+        trailing: Icon(Icons.keyboard_arrow_right),
       ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text(
-                '15* Clear',
-                style: TextStyle(color: Colors.deepPurple),
-              )
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text(
-                'Voronezhskaya obl, Voronezh',
-                style: TextStyle(color: Colors.grey),
-              )
-            ],
-          ),
-        ],
-      )
-    ],
-  );
-}
-
-Wrap _temperatureForecast() {
-  return Wrap(
-    spacing: 10,
-    children: List.generate(
-      8,
-      (int index) => Chip(
-        label: Text(
-          '${index + 20}C',
-          style: TextStyle(fontSize: 15),
-        ),
-        avatar: Icon(Icons.wb_cloudy, color: Colors.blue.shade300),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(4),
-          side: BorderSide(color: Colors.grey),
-        ),
-        backgroundColor: Colors.grey.shade100,
-      ),
-    ),
-  );
-}
-
-Row _footerRatings() {
-  var stars = Row(
-    mainAxisSize: MainAxisSize.min,
-    children: <Widget>[
-      Icon(Icons.star, size: 15, color: Colors.yellow[600]),
-      Icon(Icons.star, size: 15, color: Colors.yellow[600]),
-      Icon(Icons.star, size: 15, color: Colors.yellow[600]),
-      Icon(Icons.star, size: 15, color: Colors.black),
-      Icon(Icons.star, size: 15, color: Colors.black),
-    ],
-  );
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[
-      Text('Info with openweather', style: TextStyle(fontSize: 15)),
-      stars,
     ],
   );
 }
