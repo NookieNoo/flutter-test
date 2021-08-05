12e338f07607f7b99f9a5d7e066a0b273809543f
NookieNoo <biv0208@yandex.ru>
2021-08-05 16:47:16 +0300

    Задание с Provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
+import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
@@ -16,53 +17,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider<CountProvider>.value(value: CountProvider()),
-          FutureProvider<List<User>>(
-            create: (_) async => UserProvider().loadUserData(),
-            initialData: [],
-          ),
-          StreamProvider(
-              create: (_) => EventProvider().intStream(), initialData: 0),
-        ],
-        child: DefaultTabController(
-          length: 3,
-          child: DefaultTabController(
-            length: 3,
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text("Provider Demo"),
-                centerTitle: true,
-                bottom: TabBar(
-                  tabs: <Widget>[
-                    Tab(icon: Icon(Icons.add)),
-                    Tab(icon: Icon(Icons.person)),
-                    Tab(icon: Icon(Icons.message)),
-                  ],
-                ),
-              ),
-              body: TabBarView(
-                children: <Widget>[
-                  MyCountPage(),
-                  MyUserPage(),
-                  MyEventPage(),
-                ],
-              ),
-            ),
-          ),
-        ),
+      home: ChangeNotifierProvider<ColorProvider>(
+        child: MyColorPage(),
+        create: (context) => ColorProvider(),
       ),
     );
   }
 }
 
-class MyCountPage extends StatelessWidget {
+class MyColorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CountProvider _state = Provider.of<CountProvider>(context);
+    ColorProvider _state = Provider.of<ColorProvider>(context);
 
     return Scaffold(
+      appBar: AppBar(
+        title: Consumer<ColorProvider>(builder: (context, value, child) {
+          return Text(
+            'Test Provider',
+            style: TextStyle(color: value.titleColor),
+          );
+        }),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -70,27 +46,20 @@ class MyCountPage extends StatelessWidget {
             Text('ChangeNotifierProvider Example',
                 style: TextStyle(fontSize: 20)),
             SizedBox(height: 50),
-            Text('${_state.counterValue}',
-                style: Theme.of(context).textTheme.headline4),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.remove),
-                  color: Colors.red,
-                  onPressed: () => _state._decrementCount(),
-                ),
-                Consumer<CountProvider>(
-                  builder: (context, value, child) {
-                    return IconButton(
-                      icon: Icon(Icons.add),
-                      color: Colors.green,
-                      onPressed: () => value._incrementCount(),
-                    );
-                  },
-                ),
-              ],
+            AnimatedContainer(
+              duration: Duration(seconds: 1),
+              color: _state.blockColor,
+              width: 150,
+              height: 150,
             ),
+            Consumer<ColorProvider>(builder: (context, value, child) {
+              return Switch(
+                value: value.isSwitchedValue,
+                onChanged: value._changeColors,
+                activeTrackColor: Colors.lightGreenAccent,
+                activeColor: Colors.green,
+              );
+            }),
           ],
         ),
       ),
@@ -98,120 +67,23 @@ class MyCountPage extends StatelessWidget {
   }
 }
 
-class MyUserPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Text('FutureProvider Example, users loaded from a File',
-              style: TextStyle(fontSize: 17)),
-        ),
-        Consumer<List<User>>(
-        // Consumer(
-          builder: (context, List<User> users, _) {
-            return Expanded(
-              child: users.isEmpty
-                  ? Container(child: Center(child: CircularProgressIndicator()))
-                  : ListView.builder(
-                      itemCount: users.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 50,
-                          color: Colors.grey[(index * 200) % 400],
-                          child: Center(
-                            child: Text(
-                                '${users[index].firstName} ${users[index].lastName} | ${users[index].website}'),
-                          ),
-                        );
-                      },
-                    ),
-            );
-          },
-        ),
-      ],
-    );
-  }
-}
-
-// Event page (counting)
-class MyEventPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var _value = Provider.of<int>(context);
-    return Container(
-        child: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('StreamProvider Example', style: TextStyle(fontSize: 20)),
-        SizedBox(height: 50),
-        Text('${_value.toString()}', style: Theme.of(context).textTheme.headline4)
-      ],
-    )));
-  }
-}
-
 // CountProvider (ChangeNotifier)
-class CountProvider extends ChangeNotifier {
-  int _count = 0;
-  int get counterValue => _count;
+class ColorProvider extends ChangeNotifier {
+  Color _titleColor =
+      Colors.primaries[math.Random().nextInt(Colors.primaries.length)];
+  Color _blockColor =
+      Colors.primaries[math.Random().nextInt(Colors.primaries.length)];
+  bool _isSwitched = false;
+  Color get titleColor => _titleColor;
+  Color get blockColor => _blockColor;
+  bool get isSwitchedValue => _isSwitched;
 
-  void _incrementCount() {
-    _count++;
+  void _changeColors(bool isSwitched) {
+    _titleColor =
+        Colors.primaries[math.Random().nextInt(Colors.primaries.length)];
+    _blockColor =
+        Colors.primaries[math.Random().nextInt(Colors.primaries.length)];
+    _isSwitched = isSwitched;
     notifyListeners();
   }
-
-  void _decrementCount() {
-    _count--;
-    notifyListeners();
-  }
-}
-
-// UserProvider (Future)
-class UserProvider {
-  final String _dataPath = "assets/users.json";
-  List<User> users = [];
-
-  Future<String> loadAsset() async {
-    return await Future.delayed(Duration(seconds: 2), () async {
-      return await rootBundle.loadString(_dataPath);
-    });
-  }
-
-  Future<List<User>> loadUserData() async {
-    var dataString = await loadAsset();
-    Map<String, dynamic> jsonUserData = jsonDecode(dataString);
-    users = UserList.fromJson(jsonUserData['users']).users;
-    return users;
-  }
-}
-
-// EventProvider (Stream)
-class EventProvider {
-  Stream<int> intStream() {
-    Duration interval = Duration(seconds: 2);
-    return Stream<int>.periodic(interval, (int _count) => _count++);
-  }
-}
-
-// User Model
-class User {
-  final String firstName, lastName, website;
-  const User(this.firstName, this.lastName, this.website);
-
-  User.fromJson(Map<String, dynamic> json)
-      : this.firstName = json['first_name'],
-        this.lastName = json['last_name'],
-        this.website = json['website'];
-}
-
-// User List Model
-class UserList {
-  final List<User> users;
-  UserList(this.users);
-
-  UserList.fromJson(List<dynamic> usersJson)
-      : users = usersJson.map((user) => User.fromJson(user)).toList();
 }
