9ca301ec652450e53ea4225bfa92cdab5cf0004a
NookieNoo <biv0208@yandex.ru>
2021-09-11 21:20:31 +0300

    Bloc на потоках

--- lib/main.dart
+++ lib/main.dart
@@ -1,208 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/db/database.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 
-import 'model/student.dart';
+import 'color_bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SQLite CRUD Demo',
-      home: StudentPage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class StudentPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
-  _StudentPageState createState() => _StudentPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _StudentPageState extends State<StudentPage> {
-  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
-  final _studentNameController = TextEditingController();
-
-  Future<List<Student>>? _studentsList;
-  String? _studentName;
-  bool isUpdate = false;
-  int? studentIdForUpdate;
+class _MyHomePageState extends State<MyHomePage> {
+  ColorBloc _bloc = ColorBloc();
 
   @override
-  void initState() {
-    super.initState();
-    updateStudentList();
-  }
-
-  updateStudentList() {
-    setState(() {
-      _studentsList = DBProvider.db.getStudents();
-    });
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SQLite CRUD Demo'),
+        title: Text('Bloc Streams'),
         centerTitle: true,
-        backgroundColor: Colors.black,
       ),
-      body: Column(
+      body: Center(
+        child: StreamBuilder(
+          stream: _bloc.outputStateStream,
+          initialData: Colors.red,
+          builder: (context, snapshot) {
+            return AnimatedContainer(
+              height: 100,
+              width: 100,
+              color: snapshot.data as Color,
+              duration: Duration(milliseconds: 500),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Row(
         children: <Widget>[
-          Form(
-            key: _formStateKey,
-            autovalidate: true,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value?.isEmpty ?? false) {
-                        return 'Please Enter Student Name';
-                      }
-                      if (value?.trim() == "")
-                        return "Only Space is Not Valid!!!";
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _studentName = value;
-                    },
-                    controller: _studentNameController,
-                    decoration: InputDecoration(
-                      focusedBorder: new UnderlineInputBorder(
-                        borderSide: new BorderSide(
-                            color: Colors.greenAccent,
-                            width: 2,
-                            style: BorderStyle.solid),
-                      ),
-                      labelText: "Student Name",
-                      icon: Icon(
-                        Icons.people,
-                        color: Colors.black,
-                      ),
-                      fillColor: Colors.white,
-                      labelStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          FloatingActionButton(
+              backgroundColor: Colors.red,
+              onPressed: () {
+                _bloc.inputEventSink.add(ColorEvent.event_red);
+              }),
+          SizedBox(
+            width: 10,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                color: Colors.green,
-                child: Text(
-                  (isUpdate ? 'UPDATE' : 'ADD'),
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {
-                  if (isUpdate) {
-                    if (_formStateKey.currentState!.validate()) {
-                      _formStateKey.currentState!.save();
-                      DBProvider.db
-                          .updateStudent(
-                              Student(studentIdForUpdate, _studentName ?? ''))
-                          .then((data) {
-                        setState(() {
-                          isUpdate = false;
-                        });
-                      });
-                    }
-                  } else {
-                    if (_formStateKey.currentState!.validate()) {
-                      _formStateKey.currentState!.save();
-                      DBProvider.db.insertStudent(Student(null, _studentName ?? ''));
-                    }
-                  }
-                  _studentNameController.text = '';
-                  updateStudentList();
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red, // background
-                  textStyle: TextStyle(color: Colors.white),
-                ),
-                child: Text(
-                  (isUpdate ? 'CANCEL UPDATE' : 'CLEAR'),
-                ),
-                onPressed: () {
-                  _studentNameController.text = '';
-                  setState(() {
-                    isUpdate = false;
-                    studentIdForUpdate = null;
-                  });
-                },
-              ),
-            ],
-          ),
-          const Divider(
-            height: 5.0,
-          ),
-          Expanded(
-            child: FutureBuilder(
-                future: _studentsList,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return generateList(snapshot.data as List<Student>);
-                  }
-                  if (snapshot.data == null || (snapshot.data as List<Student>).length == 0) {
-                    return Text('No Data Found');
-                  }
-                  return CircularProgressIndicator();
-                }),
-          ),
+          FloatingActionButton(
+              backgroundColor: Colors.green,
+              onPressed: () {
+                _bloc.inputEventSink.add(ColorEvent.event_green);
+              }),
         ],
+        mainAxisAlignment: MainAxisAlignment.end,
       ),
-    );
-  }
-
-  SingleChildScrollView generateList(List<Student> students) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: DataTable(
-            columns: [
-              DataColumn(label: Text('NAME')),
-              DataColumn(label: Text('DELETE')),
-            ],
-            rows: students
-                .map(
-                  (student) => DataRow(cells: [
-                    DataCell(Text(student.name), onTap: () {
-                      setState(() {
-                        isUpdate = true;
-                        studentIdForUpdate = student.id;
-                      });
-                      _studentNameController.text = student.name;
-                    }),
-                    DataCell(
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          DBProvider.db.deleteStudent(student.id);
-                          updateStudentList();
-                        },
-                      ),
-                    ),
-                  ]),
-                )
-                .toList(),
-          )),
     );
   }
 }
